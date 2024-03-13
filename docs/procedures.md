16420beccbbfbe81fef2c9c1708fba8e45511ea8
Bruce Li <isanoob1897@outlook.com>
2024-03-13 15:29:55 +0000

    add

---
+++ docs/procedures.md
@@ -0,0 +1,6 @@
+# Procedures
+A procedure is a function but can't take arguments or return a value. They can be declared with the `procedure` keyword. For example:
+
+    procedure hi() {
+        println "hi";
+    }
--- src/interpret.py
+++ src/interpret.py
@@ -3,6 +3,7 @@ import stdlib
 
 mode = 0
 var = {}
+procedures = {}
 
 def interpret(line):
     # set as global variable
@@ -13,6 +14,10 @@ def interpret(line):
             mode = 1
         elif line.startswith("//"):
             return
+        elif line.startswith("procedure "):
+            mode = 2
+            line = line.replace("{", "")
+            name = line.replace(" ", "")
         elif "=" in line:
             stdlib.variables(line, var)
         elif len(line) < 1 or line == " ":
@@ -20,12 +25,18 @@ def interpret(line):
         else:
             print(f"Lascal: Syntax Error: Statements outside main at [{line}].")
             return
-    else:
+    elif mode == 1:
         if line.startswith("end."):
             mode = 0
         elif len(line) < 1 or line == " ":
             return
         else:
             process.process(line, var, 1)
-            
+    elif mode == 2:
+        if line.replace(" ", "") == name:
+            pass
+        elif line.startswith("}"):
+            mode = 0
+        else:
+            procedures[name] = ':' + line
 
