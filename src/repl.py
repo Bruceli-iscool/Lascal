a24e2533ae3b7b4eea28ba01f50d4fd8f0aa09e1
Bruce Li <isanoob1897@outlook.com>
2024-03-14 19:20:14 +0000

    add

--- src/repl.py
+++ src/repl.py
@@ -8,7 +8,7 @@ def repl():
         if userinput.startswith("q()"):
             break
         elif userinput.startswith("infoR()"):
-            print("REPL for Lascal with Lascal version 1.0. REPL version 1.0.0")
+            print("REPL for Lascal with Lascal version 1.1. REPL version 1.0.0")
         elif userinput.startswith("HELP"):
             print("List of commands:\nANY Lascal Snippet: returns output.\nq(): Exits REPL and returns to interpreter main.\ninfoR(): Release info.\nCtrl-D: Exit Lascal.\nLicense(): Display License.")
         elif userinput.startswith("License()"):
